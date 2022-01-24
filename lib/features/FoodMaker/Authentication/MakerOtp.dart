e91f39bf867b7b87e272a717c26ad40c048c4ede
Krsikx-Internship <krsikxbackend@gmail.com>
2022-01-24 16:35:37 +0530

    OTP Authentication completed

---
+++ lib/features/FoodMaker/Authentication/MakerOtp.dart
@@ -0,0 +1,142 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:food_app/routes/constants.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+
+class MakerOtp extends StatefulWidget {
+  final String phone;
+  MakerOtp(this.phone);
+  @override
+  _MakerOtpState createState() => _MakerOtpState();
+}
+
+class _MakerOtpState extends State<MakerOtp> {
+  late String _verificationCode;
+  final TextEditingController _otpController = TextEditingController();
+  final FocusNode _otpFocusNode = FocusNode();
+
+  final BoxDecoration otpDecoration = BoxDecoration(
+    color: const Color.fromRGBO(43, 46, 66, 1),
+    borderRadius: BorderRadius.circular(10.0),
+    border: Border.all(
+      color: const Color.fromRGBO(126, 203, 224, 1),
+    ),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _verifyPhone();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('OTP Verification'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 40),
+            child: Center(
+              child: Text(
+                'Verify ' + widget.phone,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: PinCodeTextField(
+              autoFocus: true,
+              enablePinAutofill: true,
+              focusNode: _otpFocusNode,
+              appContext: context,
+              length: 6,
+              obscureText: false,
+              animationType: AnimationType.fade,
+              pinTheme: PinTheme(
+                shape: PinCodeFieldShape.box,
+                borderRadius: BorderRadius.circular(5),
+                fieldHeight: 50,
+                fieldWidth: 40,
+                activeFillColor: Colors.white,
+              ),
+              animationDuration: Duration(milliseconds: 300),
+              backgroundColor: Colors.blue.shade50,
+              enableActiveFill: true,
+              controller: _otpController,
+              onCompleted: (otp) async {
+                try {
+                  print("1");
+                  await FirebaseAuth.instance
+                      .signInWithCredential(PhoneAuthProvider.credential(
+                          verificationId: _verificationCode, smsCode: otp))
+                      .then((value) async {
+                    if (value.user != null) {
+                      print('before adding to database');
+                      makerRef.doc(widget.phone).set({
+                        'phoneNo': widget.phone,
+                        'name': '',
+                        'address': '',
+                        'pincode': ''
+                      });
+                      Navigator.pushNamedAndRemoveUntil(
+                          context, restaurantDetailRoute, (route) => false);
+                    }
+                  });
+                } catch (e) {
+                  FocusScope.of(context).unfocus();
+                  Fluttertoast.showToast(
+                      msg: "Invalid OTP",
+                      toastLength: Toast.LENGTH_LONG,
+                      gravity: ToastGravity.BOTTOM,
+                      timeInSecForIosWeb: 1,
+                      backgroundColor: Colors.grey,
+                      textColor: Colors.white,
+                      fontSize: 18.0);
+                }
+              },
+              onChanged: (String value) {},
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  _verifyPhone() async {
+    await FirebaseAuth.instance.verifyPhoneNumber(
+      phoneNumber: widget.phone,
+      verificationCompleted: (PhoneAuthCredential credential) async {
+        print("2");
+        await FirebaseAuth.instance
+            .signInWithCredential(credential)
+            .then((value) async {
+          if (value.user != null) {
+            Navigator.pushNamedAndRemoveUntil(
+                context, restaurantDetailRoute, (route) => false);
+          }
+        });
+      },
+      verificationFailed: (FirebaseAuthException e) {
+        print(e.message);
+      },
+      codeSent: (verificationID, resendToken) {
+        setState(() {
+          _verificationCode = verificationID;
+        });
+      },
+      codeAutoRetrievalTimeout: (String verificationID) {
+        setState(() {
+          _verificationCode = verificationID;
+        });
+      },
+      timeout: Duration(seconds: 120),
+    );
+  }
+}
